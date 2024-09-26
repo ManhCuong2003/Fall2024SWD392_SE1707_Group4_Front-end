3345a75fc6bc88ef1a041d8f9aa6f7ef4e75382a
Thanh Tu <bttu2002@gmail.com>
2024-09-26 18:11:29 +0700

    add Login function

--- src/pages/commonPage/Register/Register.jsx
+++ src/pages/commonPage/Register/Register.jsx
@@ -3,27 +3,29 @@ import Logo from '../../../assets/Logo/LogoImg.png'
 import './style.scss'
 
 
- function Register() {
+function Register() {
   return (
-    <div className='container'>
+    <div className='containerr'>
       <div className="formm">
-        <img src={Logo}/>
+        <img src={Logo} />
         <h2>Create Account</h2>
-        <p className='emaill'>Full name</p>
-        <input type="text" placeholder='Enter your full name'/>
-        <p className='emaill'>User name</p>
-        <input type="text" placeholder='Enter username'/>
-        <p className='emaill'>Email</p>
-        <input type="text" placeholder='Enter your email'/>
-        <p className='pass'>Password</p>
-        <input type="password" placeholder='Password' />
-        <p className='pass'>Enter password again</p>
-        <input type="password" placeholder='Confirm Password' />
-        <p className='emaill'>Address</p>
-        <input type="text" placeholder='Enter your address'/>
-        <p className='emaill'>Phone number</p>
-        <input type="text" placeholder='Enter your phone number'/>
-        <button className='signin' style={{background:'#333333'}}>Sign up</button>
+        <div className="groupform">
+          <label htmlFor="">Full name</label>
+          <input type="text" placeholder='Enter your full name' />
+          <label htmlFor="">User name</label>
+          <input type="text" placeholder='Enter username' />
+          <label htmlFor="">Email</label>
+          <input type="text" placeholder='Enter your email' />
+          <label htmlFor="">Password</label>
+          <input type="password" placeholder='Password' />
+          <label htmlFor="">Confirm password</label>
+          <input type="password" placeholder='Confirm Password' />
+          <label htmlFor="">Address</label>
+          <input type="text" placeholder='Enter your address' />
+          <label htmlFor="">Phone number</label>
+          <input type="text" placeholder='Enter your phone number' />
+        </div>
+        <button className='signin' style={{ background: '#333333' }}>Sign up</button>
       </div>
     </div>
   );
