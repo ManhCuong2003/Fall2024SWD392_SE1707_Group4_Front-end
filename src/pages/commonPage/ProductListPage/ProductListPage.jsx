b0d93f4ec4cfa9168a7361b909c943920fa1877a
Thanh Tu <bttu2002@gmail.com>
2024-09-30 15:54:17 +0700

    updated ProductListPage

--- src/pages/commonPage/ProductListPage/ProductListPage.jsx
+++ src/pages/commonPage/ProductListPage/ProductListPage.jsx
@@ -5,38 +5,43 @@ import koi from '../../../assets/images/koi.png'
 import { FaShoppingCart, FaHeart } from "react-icons/fa";
 import Footer from '../../../layouts/Footer/Footer.jsx'
 import CustomerSideBar from '../../../layouts/CustomerSideBar/CustomerSideBar.jsx';
+import Nav from '../../../layouts/Header/Nav.jsx';
 
 
 const ProductListPage = () => {
 
     return (
-        <div className="product-list-page">
+        <div>
+            <Nav />
+            <div className="product-list-page">
 
-            <h2>CÁC SẢN PHẨM BÁN LẺ CỦA SHOP</h2>
-            <div className="product-list">
-                <CustomerSideBar />
-                <div className="product-content">
-                    <input type="search" placeholder='Search item' />
-                    <div className="product">
-                        <div className="fish-box">
-                            <img src={koi} />
-                            <h3>Kohaku</h3>
-                            <p>Tuổi: 3 tháng</p>
-                            <p>Giới tính: đực</p>
-                            <p>Màu sắc: trắng, đỏ</p>
-                            <h5>Giá: 2 tỷ VNĐ</h5>
-                            <div className="interact-shopping">
-                                <button>
-                                    <FaShoppingCart style={{ fontSize: '24px', color: '#A58361' }} />
-                                </button>
-                                <button>
-                                    <FaHeart style={{ fontSize: '24px', color: '#A58361' }} />
-                                </button>
+                <h2>CÁC SẢN PHẨM BÁN LẺ CỦA SHOP</h2>
+                <div className="product-list">
+                    <CustomerSideBar />
+                    <div className="product-content">
+                        <input type="search" placeholder='Search item' />
+                        <div className="product">
+                            <div className="fish-box">
+                                <img src={koi} />
+                                <h3>Kohaku</h3>
+                                <p>Tuổi: 3 tháng</p>
+                                <p>Giới tính: đực</p>
+                                <p>Màu sắc: trắng, đỏ</p>
+                                <h5>Giá: 2 tỷ VNĐ</h5>
+                                <div className="interact-shopping">
+                                    <button>
+                                        <FaShoppingCart style={{ fontSize: '24px', color: '#A58361' }} />
+                                    </button>
+                                    <button>
+                                        <FaHeart style={{ fontSize: '24px', color: '#A58361' }} />
+                                    </button>
+                                </div>
                             </div>
-                        </div>
 
+                        </div>
                     </div>
                 </div>
+
             </div>
             <Footer />
         </div>
